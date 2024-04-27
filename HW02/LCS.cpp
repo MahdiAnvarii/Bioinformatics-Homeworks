4e99c9bbeb8a8b588d7e32193a3af25cccc90e9a
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-27 22:36:32 +0330

    Update LCS.cpp

--- HW02/LCS.cpp
+++ HW02/LCS.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
+set<string> globalset;
 
-void PrintLCS(vector<vector<char>> b, string s1, int x1, int x2){
+void PrintLCS(vector<vector<char>> b, string s1, int x1, int x2,string current){
     if (x1==0 or x2==0){
+        //cout << current << endl;
+        globalset.insert(current);
         return;
     }
     if (b[x1-1][x2-1] == 'D'){
-        PrintLCS(b,s1,x1-1,x2-1);
-        cout << s1[x1-1];
+        current += s1[x1-1];
+        PrintLCS(b,s1,x1-1,x2-1,current);
     } else if (b[x1-1][x2-1] == 'L'){
-        PrintLCS(b,s1,x1,x2-1);
+        PrintLCS(b,s1,x1,x2-1,current);
+    } else if (b[x1-1][x2-1] == 'U'){
+        PrintLCS(b,s1,x1-1,x2,current);
     } else{
-        PrintLCS(b,s1,x1-1,x2);
+        PrintLCS(b,s1,x1,x2-1,current);
+        PrintLCS(b,s1,x1-1,x2,current);
     }
+
     return;
 }
 
@@ -33,14 +41,13 @@ int LCS(string s1, string s2, int x1, int x2){
                     b[i-1][j-1] = 'L';
                 } 
                 if ((c[i][j-1]==c[i-1][j])){
-                    b[i-1][j-1] = 'L';
+                    b[i-1][j-1] = 'B';
                 }
             }
         }
     }
 
-    PrintLCS(b,s1,x1,x2);
-    cout << endl;
+    PrintLCS(b,s1,x1,x2,"");
     return c[x1][x2];
 }
 
@@ -59,5 +66,10 @@ int main(){
 
     int answer;
     answer = LCS(seq1,seq2,x1,x2);
-    cout << "Your LCS length is: " << answer << endl;
+    cout << "Your LCSs length is: " << answer << endl;
+    cout << "You have " << globalset.size() << " LCSs." << endl;
+    cout << "These are your LCSs: " << endl;
+    for (auto itr = globalset.begin() ; itr != globalset.end() ; itr++){
+        cout << *itr << endl;
+    }
 }
