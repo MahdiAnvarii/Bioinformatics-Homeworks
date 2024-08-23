06a5dbfdeb30622a90a4f1d961476b9bb0156513
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-08-23 12:57:31 +0330

    Remove queries

--- Project/Queries/GH10/tblastn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create database
-makeblastdb -in y5.final.contigs.fa -dbtype nucl -out xylanase_db
-
-# Run tblastn
-tblastn -query GH10_AA.fasta -db new_xylanase -out GH10_result.out -evalue 1e-5 -num_threads 7
